0d6ae5ce21e71361f436ec4ede5b57a2c8b4774a
Mihai Ichim <108091752+mihaiichim74@users.noreply.github.com>
2024-02-15 12:21:49 +0100

    Create cert-all.sh

---
+++ cert-all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+token=$(cat /root/.cloudns-token)
+user=$(echo $token | cut -d '|' -f 1)
+pass=$(echo $token | cut -d '|' -f 2)
+
+url="https://api.cloudns.net/dns/list-zones.json?auth-id=${user}&auth-password=${pass}&page=1&rows-per-page=100"
+zones=$(curl $url | jq -r '.[] | .name' 2>/dev/null)
+
+for zone in $zones
+do
+  domainname=$( echo $zone | rev | cut -d'.' -f 1,2 | rev)
+  certdir=/etc/letsencrypt/live/$zone
+  if [ -d $certdir ] ; then
+    echo -n "."
+  else
+    certbot certonly --manual \
+      --preferred-challenges=dns \
+      --manual-auth-hook /usr/local/bin/certbot-auth.sh \
+      --manual-cleanup-hook /usr/local/bin/certbot-cleanup.sh \
+      -d $zone -d *.$zone
+  fi
+done
